49de371a04dbd7605dfa7c4ce0b6eb17af4d3d9b
santosh <santus444@gmail.com>
2015-06-10 22:46:42 -0400

    1) Configured the Spotify API dependency in gradle build. 2) Built the list view and list item view layouts.

--- app/src/main/java/com/santoshmandadi/spotifystreamer/app/MainActivityFragment.java
+++ app/src/main/java/com/santoshmandadi/spotifystreamer/app/MainActivityFragment.java
@@ -18,6 +18,9 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+
+        return rootView;
     }
 }
